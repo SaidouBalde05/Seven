64e6d9e3967fe2a7c86ea878214edf0991623ebe
SaidouBalde05 <baldeemamadousaidou20@gmail.com>
2024-06-03 13:38:43 +0000

    mdfctn

--- TS/homme.ts
+++ TS/homme.ts
@@ -21,3 +21,27 @@ document.addEventListener("DOMContentLoaded", function() {
         }, index * 200);  // Ajustez le délai (200ms) selon vos préférences
     });
   });
+
+  window.onload = () => {
+    const logos = document.querySelectorAll('.image');
+    const overlay = document.getElementById('overlay');
+    const overlayImage = document.getElementById('overlayImage');
+    const closeBtn = document.querySelector('#overlay .close');
+
+    logos.forEach(logo => {
+        logo.addEventListener('click', () => {
+            if (overlay && overlayImage) {
+                overlayImage.setAttribute('src', (logo as HTMLImageElement).src);
+                overlay.style.display = 'flex';
+            }
+        });
+    });
+
+    if (overlay) {
+        overlay.addEventListener('click', (event) => {
+            if (event.target === overlay || event.target === closeBtn) {
+                overlay.style.display = 'none';
+            }
+        });
+    }
+};
